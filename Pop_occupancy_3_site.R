b7cce5daa84334d911d10841a20589a172f8f1e7
trasea986 <trasea986@hotmail.com>
2021-02-18 20:26:42 -0800

    new map script viz for allele freq

--- Pop_occupancy_3_site.R
+++ Pop_occupancy_3_site.R
@@ -1,6 +1,6 @@
 # script to determine occupancy
 
-setwd("D:/OneDrive/GEM3_PostDoc/Agent-Based-Models/3_site_proj/outputs/dry_creek_sel")
+setwd("../outputs")
 
 library(tidyverse)
 library(cowplot)
@@ -10,51 +10,88 @@ library(rgeos)
 library(maptools)
 
 
-data_counts = list.files(pattern = "ind*", 
-                         full.names = TRUE, 
-                         recursive = TRUE, 
-                         include.dirs = TRUE) %>% 
-  map_df(function(x) read_csv(x, col_types = cols(.default = "c")) %>% mutate(filename=gsub(".csv","",basename(x))))
+output_files = list.files(pattern = paste(".csv", sep=''), 
+                          full.names = TRUE, 
+                          recursive = TRUE, 
+                          include.dirs = TRUE) %>% 
+  map_df(function(x) read_csv(x, col_types = cols(.default = "c")) %>% mutate(filename=paste(dirname((x)),basename(x),sep="/")))
 
-#create year column from file name
-data_df <- separate(data = data_counts, col = filename, into = c('Rest', 'Year'), sep = "ind")
 
-#remove rows from the initialization, maybe year 1
-data_df <- subset(data_df, data_df$Year != -1)
-data_df$Year <- as.numeric(data_df$Year)
+
+
+#create year and run column from file name
+data_df <- separate(data = output_files, col = filename, into = c('junk', 'run', 'junk1', 'junk2', 'year'), sep = "/")
+
+data_df <- separate(data = data_df, col = year, into = c('ind', 'year'), sep = "d")
+data_df <- separate(data = data_df, col = year, into = c('year', 'junk3'), sep = ".cs")
+data_df <- separate(data = data_df, col = run, into = c('loc', 'run'), sep = "_")
+
+#remove rows from the initialization, initial pop
+data_df <- subset(data_df, data_df$year != -1)
+data_df$junk <- NULL
+data_df$junk1 <- NULL
+data_df$junk2 <- NULL
+data_df$junk3 <- NULL
+data_df$ID <- NULL
+data_df$sex <- NULL
+data_df$size <- NULL
+data_df$mature <- NULL
+data_df$newmature <- NULL
+data_df$layeggs <- NULL
+data_df$capture <- NULL
+data_df$infection <- NULL
+data_df$Hindex <- NULL
+data_df$Species <- NULL
+data_df$recapture <- NULL
+data_df$SubPatchID <- NULL
+data_df$age <- NULL
+data_df$CDist <- NULL
+data_df$loc <- as.factor(data_df$loc)
+data_df$run <- as.factor(data_df$run)
+data_df$year <- as.numeric(data_df$year)
 data_df$XCOORD <- as.numeric(data_df$XCOORD)
 data_df$YCOORD <- as.numeric(data_df$YCOORD)
 
-summary <- data_df %>%
-  group_by(Year) %>%
-  count(PatchID) %>%
-  count(Year)
 
 
-summary_clear <- summary %>% filter(n > 1)
-
-summary_clear$Year <- as.integer(summary_clear$Year)
-
-summary_clear %>%
-  group_by(Year) %>%
-  summarise(n = n) %>%
-  mutate(freq = n / max(n))
-
-#to then summarize
+#summarize for maps
 
 for_viz <- data_df %>%
-group_by(PatchID, XCOORD, YCOORD, Year, .drop = FALSE) %>% 
+group_by(PatchID, XCOORD, YCOORD, year, loc, run, .drop = FALSE) %>% 
+  summarise(n = n())
+
+for_stat <- data_df %>%
+  group_by(year, loc, run, .drop = FALSE) %>% 
   summarise(n = n())
 
 #data_viz_test <- for_viz %>% filter(Year == 10)
 
-#copied coordinates from ArcGIS Pro
-#116.4234612캷 43.6655836캮 
-#116.0662912캷 43.8163286캮 
+#copied coordinates from ArcGIS Pro fro 
+#Dry Creek
+#116.4234612?W 43.6655836?N 
+#116.0662912?W 43.8163286?N 
 
-map <- get_map(c(left = -116.4234612, bottom = 43.6655836, top = 43.8163286, right = -116.0662912))
+#Mann/Keithley
+#117.0561113째W 44.3861876째N BL
+#116.7206293째W 44.6363213째N TR
 
-p <- ggmap(map)
+#Jacks
+#115.8251393째W 42.8174436째N  TR
+#116.3219521째W 42.3691728째N  BL
+
+map_dry_creek <- get_map(c(left = -116.4234612, bottom = 43.6655836, top = 43.8163286, right = -116.0662912))
+
+map_cool <- get_map(c(left = -117.0561113, bottom = 44.3861876, top = 44.6363213, right = -116.7206293))
+
+map_desert <- get_map(c(left = -116.3219521, bottom = 42.3691728, top = 42.8174436, right = -115.8251393))
+
+p_dry <- ggmap(map_dry_creek)
+p_cool <- ggmap(map_cool)
+p_desert <- ggmap(map_desert)
+
+p_dry
+p_cool
+p_desert
 
 #change UTM to LONG LAT and add back in n
 coordinates(for_viz) <- ~XCOORD+YCOORD #similar to SpatialPoints
@@ -62,30 +99,38 @@ data_viz_UTM <- SpatialPoints(for_viz, proj4string=CRS("+proj=utm +zone=11 +datu
 data_viz_LONGLAT <- spTransform(data_viz_UTM, CRS("+proj=longlat +datum=WGS84"))
 data_viz_ll_df <- as.data.frame(data_viz_LONGLAT)
 data_viz_ll_df$n <- for_viz$n
-data_viz_ll_df$Year <- for_viz$Year
+data_viz_ll_df$year <- for_viz$year
+data_viz_ll_df$loc <- for_viz$loc
+data_viz_ll_df$run <- for_viz$run
 
-#remove points for easier visualization
+#sep by location
+data_viz_ll_df_dry <- data_viz_ll_df %>%
+  filter(loc == "dry")
+data_viz_ll_df_cool <- data_viz_ll_df %>%
+  filter(loc == "cool")
+data_viz_ll_df_desert <- data_viz_ll_df %>%
+  filter(loc == "desert")
 
 
 
 #add in stream layer data
-stream <- readOGR("../../data_gis/NorWeST_PredictedStreamTempLines_MiddleSnake.shp")
+stream <- readOGR("../data_gis/NorWeST_PredictedStreamTempLines_MiddleSnake.shp")
 stream_repro <- spTransform(stream, CRS("+proj=longlat +datum=WGS84"))
 stream_fort <- fortify(stream_repro)
 
-data_viz_ll_df <- data_viz_ll_df %>%
-  filter(Year != 10)
 
-for (i in unique(data_viz_ll_df$Year)) {
 
-  Year = i 
+#dry creek test
+for (i in unique(data_viz_ll_df_dry$year)) {
+  year = i 
   
-  data_plot <- data_viz_ll_df %>%
-    filter(Year == i)
+  data_plot <- data_viz_ll_df_dry %>%
+    filter(year == i)
   
-  myplot <- p + 
+  myplot <- p_dry + 
     geom_path(data = stream_fort, aes(long, lat, group = group), color = "lightblue") +
   geom_point(data = data_plot, aes(x = XCOORD, y = YCOORD, size = n, alpha = n), color = "black", fill = "red", shape = 21) +
+    facet_wrap(~run) +
   scale_size_continuous(range = c(1, 8)) +
   scale_alpha_continuous(range = c(0.25, .75)) +
   theme(legend.position = "none") +
@@ -96,5 +141,55 @@ for (i in unique(data_viz_ll_df$Year)) {
   theme(legend.position = "none")
   
   ggsave(myplot, filename=paste("DryCreek",i,".png",sep=""),dpi = 400, width = 10, height = 8, units = "in")
-  
 }
+
+
+#cool creek test
+for (i in unique(data_viz_ll_df_cool$year)) {
+  year = i 
+  
+  data_plot <- data_viz_ll_df_cool %>%
+    filter(year == i)
+  
+  myplot <- p_cool + 
+    geom_path(data = stream_fort, aes(long, lat, group = group), color = "lightblue") +
+    geom_point(data = data_plot, aes(x = XCOORD, y = YCOORD, size = n, alpha = n), color = "black", fill = "red", shape = 21) +
+    facet_wrap(~run) +
+    scale_size_continuous(range = c(1, 8)) +
+    scale_alpha_continuous(range = c(0.25, .75)) +
+    theme(legend.position = "none") +
+    ylab("Latitude") +
+    xlab("Longitude") +
+    ggtitle(paste("Keithley/Mann Creek",i,sep = " ")) +
+    theme_bw(base_size = 14)+
+    theme(legend.position = "none")
+  
+  ggsave(myplot, filename=paste("Cool",i,".png",sep=""),dpi = 400, width = 10, height = 8, units = "in")
+}
+
+
+#desert creek test
+for (i in unique(data_viz_ll_df_desert$year)) {
+  year = i 
+  
+  data_plot <- data_viz_ll_df_desert %>%
+    filter(year == i)
+  
+  myplot <- p_desert + 
+    geom_path(data = stream_fort, aes(long, lat, group = group), color = "lightblue") +
+    geom_point(data = data_plot, aes(x = XCOORD, y = YCOORD, size = n, alpha = n), color = "black", fill = "red", shape = 21) +
+    facet_wrap(~run) +
+    scale_size_continuous(range = c(1, 8)) +
+    scale_alpha_continuous(range = c(0.25, .75)) +
+    theme(legend.position = "none") +
+    ylab("Latitude") +
+    xlab("Longitude") +
+    ggtitle(paste("Jack's Creek",i,sep = " ")) +
+    theme_bw(base_size = 14)+
+    theme(legend.position = "none")
+  
+  ggsave(myplot, filename=paste("Desert",i,".png",sep=""),dpi = 400, width = 10, height = 8, units = "in")
+}
+
+
+
---
+++ Viz_pop_num.R
@@ -0,0 +1,169 @@
+#script for figures for the presentation
+#start in proj directory
+
+library(tidyverse)
+library(cowplot)
+library(ggpubr)
+library(plyr)
+
+pop <- data_df %>%
+  group_by(year, loc, run) %>%
+  tally()
+
+pop$year <- as.numeric(pop$year)
+
+pop$loc <- as.factor(pop$loc)
+pop$loc <- revalue(pop$loc, c("cool"="Keithley/Mann", "dry"="Dry Creek", "desert" = "Jack's Creeks"))
+
+pop$run <- as.factor(pop$run)
+pop$run <- revalue(pop$run, c("null"="Null", "plast"="Plasticity: Habitat Selection", "sel" = "Local Adaptation: Thermal Tolerance", "sel-plast"="Combination"))
+
+
+pop$Model <- pop$run
+
+ggplot(pop, aes(x=year, y=n, color = Model)) +
+  geom_line(size=2) +
+  labs(x = "Time (years)", y = "Population Size") +
+  facet_grid(~loc) +
+  scale_color_brewer(palette="Dark2") +
+  scale_fill_brewer(palette="Dark2") +
+  xlim(20,100) +
+  scale_y_continuous(labels = scales::comma, limits = c(0, 100000)) +
+  theme_classic(base_size = 40)
+
+ggsave(filename = "pop1.png", plot = last_plot(), dpi = 400, width = 10, height = 8, units = "in")
+
+
+
+
+#convert chr to factor
+data_df$L0A0 <- as.numeric(data_df$L0A0)
+data_df$L0A1 <- as.numeric(data_df$L0A1)
+data_df$L1A0 <- as.numeric(data_df$L1A0)
+data_df$L1A1 <- as.numeric(data_df$L1A1)
+data_df$L2A0 <- as.numeric(data_df$L2A0)
+data_df$L2A1 <- as.numeric(data_df$L2A1)
+
+#going to first see what the total should be
+Population <- data_df %>%
+  group_by(year, loc, run) %>%
+  tally()
+
+Population$year <- as.numeric(Population$year)
+
+#going to make seperate dataframes here for each locus
+#sum gives the total number of alleles in the population
+data_df_L0 <- data_df %>%
+  group_by(year, loc, run) %>%
+  summarise_at(vars(L0A0:L0A1), sum, na.rm = TRUE)
+
+
+#plastic region a bit different, need the zeroes
+
+#first count the homozygotes
+data_df %>%
+  group_by(year, L1A0, L0A1, loc, run) %>%
+  tally()
+
+data_L1A1 <- data_df %>%
+  group_by(year, loc, run) %>%
+  dplyr::count(L1A1, name = "L1A1_count")
+
+data_L1A1_spread <- data_L1A1 %>%
+  spread(L1A1, L1A1_count)
+
+data_L1A0 <- data_df %>%
+  group_by(year, loc, run) %>%
+  dplyr::count(L1A0, name = "L1A0_count")
+
+data_L1A0_spread <- data_L1A0 %>%
+  spread(L1A0, L1A0_count)
+
+
+data_df_L1 <- merge(x=data_L1A0_spread, y=data_L1A1_spread, by=c("year","loc","run"), all = TRUE)
+
+head(data_df_L1)
+
+#next sum together across
+
+data_df_L1$L1A0 <- data_df_L1$'0.x' + data_df_L1$'0.y'
+#this will throw an error if all switches flipped
+data_df_L1$L1A1 <- data_df_L1$'1.x' + data_df_L1$'1.y'
+data_df_L1$L1A2 <- data_df_L1$'2.x' + data_df_L1$'2.y'
+
+#remove old rows
+data_df_L1$'0.x' <- NULL
+data_df_L1$'0.y' <- NULL
+data_df_L1$'1.x' <- NULL
+data_df_L1$'1.y' <- NULL
+data_df_L1$'2.x' <- NULL
+data_df_L1$'2.y' <- NULL
+
+
+
+
+data_df_L2 <- data_df %>%
+  group_by(year, loc, run) %>%
+  summarise_at(vars(L2A0:L2A1), sum, na.rm = TRUE)
+
+#bringing together for comparisons
+df_all <- merge(x = data_df_L0, y = data_df_L1, by=c("year", "loc", "run"), all = TRUE)
+df_all <- merge(x=df_all, y=data_df_L2, by=c("year", "loc", "run"), all = TRUE)
+
+df_all$year <- as.numeric(df_all$year)
+
+
+
+#gathering the columns of alleles
+df_all_plot <- gather(df_all, key = "Allele", value, L0A0:L2A1)
+
+#going split allele column to include locus for better graphing
+
+df_all_plot <- separate(data = df_all_plot, col = Allele, into = c('Locus', 'Allele'), sep = 2)
+
+#plasticity locus only plot
+df_plastic_plot <- subset(df_all_plot, df_all_plot$Locus != "L0" & df_all_plot$Locus != "L2")
+
+head(df_plastic_plot)
+
+
+
+#graph
+
+
+
+#rename loci then plot
+df_plot_final <- df_all_plot %>% 
+  mutate(Locus = replace(Locus, Locus == 'L0', "Adaptive")) %>%
+  mutate(Locus = replace(Locus, Locus == 'L2', "Neutral")) %>%
+  mutate(Locus = replace(Locus, Locus == 'L1', "Plastic"))
+
+df_plot_final$Locus <- as.factor(df_plot_final$Locus)
+
+df_plot_final$Locus <- factor(df_plot_final$Locus,levels(df_plot_final$Locus)[c(1,3,2)])
+
+
+
+df_plot_final <- merge(x=df_plot_final, y=Population, by=c("year", "loc", "run"), all = TRUE)
+df_plot_final$value <- df_plot_final$value / (2*df_plot_final$n)
+
+
+df_plot_final$run <- revalue(df_plot_final$run, c("null"="Null", "plast"="Plasticity", "sel" = "Local Adaptation", "sel-plast"="Combination"))
+
+for (i in unique(df_plot_final$loc)) {
+  loc = i 
+  
+  data_plot <- df_plot_final %>%
+    filter(loc == i)
+
+my_plot <- ggplot(data=data_plot, aes(x = year, y = value, color=as.factor(Allele), fill=as.factor(Allele), shape=as.factor(Allele))) +
+  geom_point(size=4, alpha=0.55) +
+  geom_line(size=1.25)+
+  facet_grid(rows = vars(run), cols = vars(Locus)) +
+  labs(x = "Time (years)", y = "Allele Proportion", color="Allele", fill="Allele", shape="Allele") +
+  ggtitle(paste("Location",i,sep = " ")) +
+  scale_color_brewer(palette = "Dark2")+
+  theme_bw(base_size=14)
+
+ggsave(my_plot, filename=paste("frequencies",i,".png",sep=""),dpi = 400, width = 10, height = 8, units = "in")
+}
